ce15f39f16dbb02a9a591b9e620898f101fcac75
Nadav Ivgi <nadav@shesek.info>
2017-11-19 17:04:59 +0200

    Store msatoshi as string

--- migrations/0000_01_invoice.js
+++ migrations/0000_01_invoice.js
@@ -1,15 +1,15 @@
 
 exports.up = db =>
   db.schema.createTable('invoice', t => {
-    t.string    ('id').primary()
-    t.string    ('peerid').notNullable()
-    t.bigInteger('msatoshi').notNullable()
-    t.string    ('rhash').notNullable()
-    t.string    ('payreq').notNullable()
-    t.bool      ('completed').notNullable()
-    t.string    ('metadata').nullable()
-    t.timestamp ('created_at').notNullable()
-    t.timestamp ('completed_at').nullable()
+    t.string   ('id').primary()
+    t.string   ('peerid').notNullable()
+    t.string   ('msatoshi').notNullable()
+    t.string   ('rhash').notNullable()
+    t.string   ('payreq').notNullable()
+    t.bool     ('completed').notNullable()
+    t.string   ('metadata').nullable()
+    t.timestamp('created_at').notNullable()
+    t.timestamp('completed_at').nullable()
   })
 
 exports.down = db =>
