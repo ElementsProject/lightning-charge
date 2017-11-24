9cb020e65e8b40bac162eeb429eb47f4a3eae50f
Nadav Ivgi <nadav@shesek.info>
2017-11-24 02:31:19 +0200

    Change {created|completed}_at fields to integer

--- migrations/0000_01_invoice.js
+++ migrations/0000_01_invoice.js
@@ -1,17 +1,17 @@
 
 exports.up = db =>
   db.schema.createTable('invoice', t => {
-    t.string   ('id').primary()
-    t.string   ('msatoshi').notNullable()
-    t.string   ('quoted_currency').nullable()
-    t.string   ('quoted_amount').nullable()
-    t.string   ('peerid').notNullable()
-    t.string   ('rhash').notNullable()
-    t.string   ('payreq').notNullable()
-    t.bool     ('completed').notNullable()
-    t.string   ('metadata').nullable()
-    t.timestamp('created_at').notNullable()
-    t.timestamp('completed_at').nullable()
+    t.string ('id').primary()
+    t.string ('msatoshi').notNullable()
+    t.string ('quoted_currency').nullable()
+    t.string ('quoted_amount').nullable()
+    t.string ('peerid').notNullable()
+    t.string ('rhash').notNullable().unique()
+    t.string ('payreq').notNullable()
+    t.bool   ('completed').notNullable()
+    t.string ('metadata').nullable()
+    t.integer('created_at').notNullable()
+    t.integer('completed_at').nullable()
   })
 
 exports.down = db =>
