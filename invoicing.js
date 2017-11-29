7a186d92f1c445b5d7bbbb65f46bfaa2472def0b
Nadav Ivgi <nadav@shesek.info>
2017-11-29 13:21:01 +0200

    Remove unused debug import, plus some minor changes

--- invoicing.js
+++ invoicing.js
@@ -1,14 +1,12 @@
 import wrap from './lib/promise-wrap'
 
-const debug = require('debug')('lightning-strike')
-
 module.exports = app => {
   const { payListen, model: { newInvoice, fetchInvoice, listInvoices } } = app
 
   app.param('invoice', wrap(async (req, res, next, id) => {
     req.invoice = await fetchInvoice(req.params.invoice)
     if (!req.invoice) return res.sendStatus(404)
-    req.invoice_expired = !req.invoice.completed && req.invoice.expires_at && req.invoice.expires_at < Date.now()/1000
+    req.invoice_expired = !req.invoice.completed && req.invoice.expires_at < Date.now()/1000
     next()
   }))
 
@@ -18,10 +16,8 @@ module.exports = app => {
   app.get('/invoice/:invoice', (req, res) =>
     res.send(req.invoice))
 
-  app.post('/invoice', wrap(async (req, res) => {
-    const invoice = await newInvoice(req.body)
-    res.status(201).send(invoice)
-  }))
+  app.post('/invoice', wrap(async (req, res) =>
+    res.status(201).send(await newInvoice(req.body))))
 
   app.get('/invoice/:invoice/wait', wrap(async (req, res) => {
     if (req.invoice.completed) return res.send(req.invoice)
@@ -32,7 +28,7 @@ module.exports = app => {
 
     if (paid) res.send(paid)
     else res.sendStatus(402)
-    // @TODO properly handle client disconnect
+    // @TODO remove listener on client disconnect
   }))
 
   app.get('/payment-stream', (req, res, next) => {
