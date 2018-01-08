51be3efad98c37ee1c02a3f0ffd77e9a8091baf0
Nadav Ivgi <nadav@shesek.info>
2018-01-08 04:59:46 +0200

    Refactor

--- webhook.js
+++ webhook.js
@@ -13,22 +13,16 @@ module.exports = (app, payListen, model, auth) => {
     res.sendStatus(201)
   }))
 
-  async function execHooks(invoice) {
-    debug('execHooks(%s)', invoice.id)
+  payListen.on('payment', async invoice => {
     const hooks = await getHooks(invoice.id)
-    debug('execHooks(%s): %j', invoice.id, hooks.map(h => h.url))
+    debug('Calling webhooks for %s: %o', invoice.id, hooks.map(h => h.url))
 
-    return Promise.all(hooks.map(hook =>
+    Promise.all(hooks.map(hook =>
       request.post(hook.url)
         .type('json').send(invoice)
         .then(res  => res.ok ? res : Promise.reject(new Error('invalid status code '+res.status)))
         .then(res  => logHook(hook.id, null, res))
         .catch(err => logHook(hook.id, err))
     ))
-  }
-
-  payListen.on('payment', invoice =>
-    execHooks(invoice)
-      .then(results => debug('%s webhook results: %j', invoice.id, results)))
-
+  })
 }
