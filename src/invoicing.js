23cdf66bcc89288710810f663622b8d16fb21846
Nadav Ivgi <nadav@shesek.info>
2018-01-10 11:23:32 +0200

    Abort long-polling for expired invoices as soon as they expire

--- src/invoicing.js
+++ src/invoicing.js
@@ -25,11 +25,12 @@ module.exports = (app, payListen, model, auth) => {
     if (req.invoice.completed) return res.send(req.invoice)
     if (req.invoice_expired)   return res.sendStatus(410)
 
-    const timeout = Math.min(+req.query.timeout || 300, 1800)*1000
-        , paid    = await payListen.register(req.params.invoice, timeout)
+    const expires_in = req.invoice.expires_at - (Date.now()/1000|0)
+        , timeout    = Math.min(+req.query.timeout || 300, expires_in, 1800)
+        , paid       = await payListen.register(req.params.invoice, timeout*1000)
 
     if (paid) res.send(paid)
-    else res.sendStatus(402)
+    else res.sendStatus(timeout == expires_in ? 410 : 402)
     // @TODO remove listener on client disconnect
   }))
 
