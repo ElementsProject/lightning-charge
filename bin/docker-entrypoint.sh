6e15cca283f3d7923d548470d0d324d19af4a479
Nadav Ivgi <nadav@shesek.info>
2018-01-19 04:46:32 +0200

    Docker: Disable pruning (still not working perfectly with c-lightning), remove redundant option

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -4,12 +4,12 @@ set -eo pipefail
 : ${NETWORK:=testnet}
 
 echo "Starting bitcoind"
-bitcoind -daemon -$NETWORK -prune=550 $BITCOIND_OPTS
+bitcoind -daemon -$NETWORK $BITCOIND_OPTS
 echo "Waiting for bitcoind to startup"
 sed '/init message: Done loading/ q' <(tail -F -n+0 ~/.bitcoin/*/debug.log 2> /dev/null)
 
 echo "Starting lightningd"
-lightningd --network=$NETWORK --port 9735 --log-file=debug.log $LIGHTNINGD_OPT &
+lightningd --network=$NETWORK --log-file=debug.log $LIGHTNINGD_OPT &
 echo "Waiting for lightningd to startup"
 sed '/Hello world/ q' <(tail -F -n+0 ~/.lightning/debug.log 2> /dev/null)
 
