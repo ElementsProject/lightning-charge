8946d01130e548fd0a8bce7741dee9758912460e
nicolas.dorier <nicolas.dorier@gmail.com>
2018-01-26 20:51:22 +0100

    Make docker-entrypoint effectively support bitcoind from another container

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -8,17 +8,30 @@ export BTC_PATH=/data/bitcoin
 : ${NETWORK:=testnet}
 : ${LIGHTNINGD_OPT:=--log-level=debug}
 : ${BITCOIND_OPT:=-debug=rpc}
+: ${BITCOIND_HOST:=127.0.0.1}
 
 [[ "$NETWORK" == "mainnet" ]] && NETWORK=bitcoin
 
-mkdir -p $BTC_PATH $LN_PATH
+mkdir -p $LN_PATH
 
 if [ -z "$SKIP_BITCOIND" ]; then
+  mkdir -p $BTC_PATH
   echo "Starting bitcoind"
   bitcoind -printtoconsole -$NETWORK -datadir=$BTC_PATH $BITCOIND_OPTS &>> /data/bitcoin.log &
-  echo "Waiting for bitcoind to startup"
-  sed '/init message: Done loading/ q' <(tail -F -n0 /data/bitcoin.log 2> /dev/null)
+  # We need to wait at least until the cookiefile is created...
+  sleep 1
+else
+  # The path should be out of /data, because /data is the volume concerning this container only
+  BTC_PATH=/root/.bitcoin
+  echo "Using external bitcoind '$BITCOIND_HOST'"
+  # LightningD depends on bitcoin-cli which use the rpcconnect argument to know the host to connect to.
+  # We can't configure LightningD to use a different host, but we can configure the underlying bitcoin-cli to use it
+  sed -i '/^rpcconnect=/d' $BTC_PATH/bitcoin.conf
+  echo "rpcconnect=$BITCOIND_HOST" >> $BTC_PATH/bitcoin.conf
 fi
+
+echo "Waiting for bitcoind to startup"
+bitcoin-cli -$NETWORK -datadir="$BTC_PATH" -rpcwait getblockchaininfo  &>> /dev/null
 
 if [ -z "$SKIP_LIGHTNINGD" ]; then
   echo "Starting lightningd"
