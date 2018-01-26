7251fdc7c59bb849f8ab24529bf9ab3511245e81
Nadav Ivgi <nadav@shesek.info>
2018-01-26 12:28:23 +0200

    Docker: normalize debug log file paths to /data/{lightning,bitcoin}.log

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -13,16 +13,16 @@ mkdir -p $BTC_PATH $LN_PATH
 
 if [ -z "$SKIP_BITCOIND" ]; then
   echo "Starting bitcoind"
-  bitcoind -daemon -$NETWORK -datadir=$BTC_PATH $BITCOIND_OPTS
+  bitcoind -printtoconsole -$NETWORK -datadir=$BTC_PATH $BITCOIND_OPTS &>> /data/bitcoin.log &
   echo "Waiting for bitcoind to startup"
-  sed '/init message: Done loading/ q' <(tail -F -n+0 $BTC_PATH/*/debug.log 2> /dev/null)
+  sed '/init message: Done loading/ q' <(tail -F -n0 /data/bitcoin.log 2> /dev/null)
 fi
 
 if [ -z "$SKIP_LIGHTNINGD" ]; then
   echo "Starting lightningd"
-  lightningd --network=$NETWORK --bitcoin-datadir=$BTC_PATH --lightning-dir=$LN_PATH --log-file=debug.log $LIGHTNINGD_OPT &
+  lightningd --network=$NETWORK --bitcoin-datadir=$BTC_PATH --lightning-dir=$LN_PATH $LIGHTNINGD_OPT &>> /data/lightning.log &
   echo "Waiting for lightningd to startup"
-  sed '/Hello world/ q' <(tail -F -n+0 $LN_PATH/debug.log 2> /dev/null)
+  sed '/Hello world/ q' <(tail -F -n0 /data/lightning.log 2> /dev/null)
 fi
 
 echo "Starting Lightning Charge"
