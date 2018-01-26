5914b8618d6def1a47b553d143a66e29aa55df2b
Nadav Ivgi <nadav@shesek.info>
2018-01-26 03:54:35 +0200

    Docker: make bitcoind and lightningd optional

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -11,15 +11,19 @@ export BTC_PATH=/data/bitcoin
 
 mkdir -p $BTC_PATH $LN_PATH
 
-echo "Starting bitcoind"
-bitcoind -daemon -$NETWORK -datadir=$BTC_PATH $BITCOIND_OPTS
-echo "Waiting for bitcoind to startup"
-sed '/init message: Done loading/ q' <(tail -F -n+0 $BTC_PATH/*/debug.log 2> /dev/null)
+if [ -z "$SKIP_BITCOIND" ]; then
+  echo "Starting bitcoind"
+  bitcoind -daemon -$NETWORK -datadir=$BTC_PATH $BITCOIND_OPTS
+  echo "Waiting for bitcoind to startup"
+  sed '/init message: Done loading/ q' <(tail -F -n+0 $BTC_PATH/*/debug.log 2> /dev/null)
+fi
 
-echo "Starting lightningd"
-lightningd --network=$NETWORK --bitcoin-datadir=$BTC_PATH --lightning-dir=$LN_PATH --log-file=debug.log $LIGHTNINGD_OPT &
-echo "Waiting for lightningd to startup"
-sed '/Hello world/ q' <(tail -F -n+0 $LN_PATH/debug.log 2> /dev/null)
+if [ -z "$SKIP_LIGHTNINGD" ]; then
+  echo "Starting lightningd"
+  lightningd --network=$NETWORK --bitcoin-datadir=$BTC_PATH --lightning-dir=$LN_PATH --log-file=debug.log $LIGHTNINGD_OPT &
+  echo "Waiting for lightningd to startup"
+  sed '/Hello world/ q' <(tail -F -n+0 $LN_PATH/debug.log 2> /dev/null)
+fi
 
 echo "Starting Lightning Charge"
 HOST=0.0.0.0 DEBUG=$DEBUG,lightning-charge,lightning-client,knex:query,knex:bindings \
