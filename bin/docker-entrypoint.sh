95a28607fc0646f6574d572468fecc0314700fec
Nadav Ivgi <nadav@shesek.info>
2018-01-27 19:39:17 +0200

    Docker: support remote bitcoind instances
    The bitcoind instance to connect to can be configured using two methods:
    
    1. Setting the `BITCOIND_URI` environment variable:
    
       $ docker run -e BITCOIND_URI=http://[user]:[pass]@[host]:[port] shesek/lightning-charge
    
       For cookie-based authentication, use the special-cased `__cookie__` username, as such:
       `BITCOIND_URI=http://__cookie__:[cookie]@[host]:[port]/`
    
    2. Mounting an external bitcoin data directory to `/etc/bitcoin`:
    
       $ docker run -v $HOME/.bitcoin:/etc/bitcoin shesek/lightning-charge
    
       This is useful if a bitcoind instance is already running on the host machine or on a separate container (see https://github.com/ElementsProject/lightning-charge/pull/19).
    
       To change the default RPC host (127.0.0.1), set `BITCOIND_RPCCONNECT_HACK=<host>`. This WILL MODIFY the external bitcoin.conf and considered a temporary workaround. See comments in code for more details.

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -1,30 +1,67 @@
 #!/bin/bash
 set -eo pipefail
 
-export DB_PATH=/data/charge.db
-export LN_PATH=/data/lightning
-export BTC_PATH=/data/bitcoin
-
 : ${NETWORK:=testnet}
 : ${LIGHTNINGD_OPT:=--log-level=debug}
 : ${BITCOIND_OPT:=-debug=rpc}
 
 [[ "$NETWORK" == "mainnet" ]] && NETWORK=bitcoin
 
-mkdir -p $BTC_PATH $LN_PATH
+if [ -d /etc/bitcoin ]; then
+  echo -n "Connecting to bitcoind configured in /etc/bitcoin... "
+  BTC_PATH=/etc/bitcoin
 
-if [ -z "$SKIP_BITCOIND" ]; then
+  # temporary workaround to allow mounting the bitcoind datadir as-is,
+  # but specify a different rpc host to connect to. this will modify
+  # the external bitcoind config file which might result in side effects.
+  # should be eventually replaced with a better solution.
+  if [ -n "$BITCOIND_RPCCONNECT_HACK" ]; then
+    sed -i '/^rpcconnect=/ d' $BTC_PATH/bitcoin.conf
+    echo "rpcconnect=$BITCOIND_RPCCONNECT_HACK" >> $BTC_PATH/bitcoin.conf
+  fi
+
+elif [ -z "$BITCOIND_URI" ]; then
   echo -n "Starting bitcoind... "
+
+  BTC_PATH=/data/bitcoin
+  mkdir -p $BTC_PATH
+
   bitcoind -printtoconsole -$NETWORK -datadir=$BTC_PATH $BITCOIND_OPTS &>> /data/bitcoin.log &
   echo -n "waiting for cookie... "
   sed --quiet '/^\.cookie$/ q' <(inotifywait -e create,moved_to --format '%f' -qmr $BTC_PATH)
-  echo -n "waiting for RPC... "
-  bitcoin-cli -$NETWORK -datadir=$BTC_PATH -rpcwait getblockchaininfo > /dev/null
-  echo "ready."
+
+elif [[ "$BITCOIND_URI" =~ ^[a-z]+:\/+(([^:]+):([^@]+))@([^:/]+):([0-9]+)/?$ ]]; then
+  echo -n "Connecting to bitcoind at ${BASH_REMATCH[4]}:${BASH_REMATCH[5]}... "
+
+  BTC_PATH=/tmp/bitcoin
+  mkdir $BTC_PATH
+
+  echo -e "$NETWORK=1\nrpconnect=${BASH_REMATCH[4]}\nrpcport=${BASH_REMATCH[5]}" > $BTC_PATH/bitcoin.conf
+
+  if [ "${BASH_REMATCH[2]}" != "__cookie__" ]; then
+    echo -e "rpcuser=${BASH_REMATCH[2]}\nrpcpassword=${BASH_REMATCH[3]}" >> $BTC_PATH/bitcoin.conf
+  else
+    [[ "$NETWORK" == "bitcoin" ]] && NET_PATH=$BTC_PATH || NET_PATH=$BTC_PATH/$NETWORK
+    mkdir -p $NET_PATH
+    echo "${BASH_REMATCH[1]}" > $NET_PATH/.cookie
+  fi
+else
+  echo >2 "Invalid bitcoind URI: $BITCOIND_URI"
+  exit 1
 fi
 
-if [ -z "$SKIP_LIGHTNINGD" ]; then
+echo -n "waiting for RPC... "
+bitcoin-cli -$NETWORK -datadir=$BTC_PATH -rpcwait getblockchaininfo > /dev/null
+echo "ready."
+
+if [ -S /etc/lightning/lightning-rpc ]; then
+  echo "Using lightningd unix socket mounted in /etc/lightning/lightning-rpc"
+  LN_PATH=/etc/lightning
+
+else
   echo -n "Starting lightningd... "
+
+  LN_PATH=/data/lightning
   lightningd --network=$NETWORK --bitcoin-datadir=$BTC_PATH --lightning-dir=$LN_PATH $LIGHTNINGD_OPT &>> /data/lightning.log &
   echo -n "waiting for startup... "
   sed --quiet '/Hello world/ q' <(tail -F -n0 /data/lightning.log 2> /dev/null)
@@ -33,4 +70,5 @@ fi
 
 echo "Starting Lightning Charge"
 HOST=0.0.0.0 DEBUG=$DEBUG,lightning-charge,lightning-client,knex:query,knex:bindings \
+DB_PATH=/data/charge.db LN_PATH=$LN_PATH \
 ./bin/charged $@ $CHARGED_OPTS
