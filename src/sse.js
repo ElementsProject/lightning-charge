70b29204996b4bf87eec640e8b02d12eb4aae841
Nadav Ivgi <nadav@shesek.info>
2018-02-20 00:44:39 +0200

    SSE: send keep-alive messages, instruct nginx not to buffer responses

--- src/sse.js
+++ src/sse.js
@@ -1,12 +1,17 @@
 module.exports = (app, payListen, auth) =>
-  app.get('/payment-stream', auth, (req, res, next) => {
+  app.get('/payment-stream', auth, (req, res) => {
     res.set({
-      'Content-Type':  'text/event-stream'
+      'X-Accel-Buffering': 'no'
     , 'Cache-Control': 'no-cache'
-    , 'Connection':    'keep-alive'
+    , 'Content-Type': 'text/event-stream'
+    , 'Connection': 'keep-alive'
     }).flushHeaders()
 
     const onPay = inv => res.write(`id:${ inv.pay_index }\ndata:${ JSON.stringify(inv) }\n\n`)
     payListen.on('payment', onPay)
-    req.on('close', _ => payListen.removeListener('payment', onPay))
+
+    const keepAlive = setInterval(_ => res.write(': keepalive\n\n'), 25000)
+
+    req.on('close', _ => (payListen.removeListener('payment', onPay)
+                        , clearInterval(keepAlive)))
   })
