4ffaa487b380ada0b71efa40e4fcf7a4af645f90
Nadav Ivgi <nadav@shesek.info>
2018-03-14 13:56:54 +0200

    Docker: shutdown the entire process when any of the background jobs exits

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -69,4 +69,8 @@ fi
 
 echo "Starting Lightning Charge"
 DEBUG=$DEBUG,lightning-charge,lightning-client \
-charged -d /data/charge.db -l $LN_PATH -i 0.0.0.0 $@ $CHARGED_OPTS
+charged -d /data/charge.db -l $LN_PATH -i 0.0.0.0 $@ $CHARGED_OPTS &
+
+# shutdown the entire process when any of the background jobs exits (even if successfully)
+wait -n
+kill -TERM $$
