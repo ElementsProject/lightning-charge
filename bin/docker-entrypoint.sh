0c6c67f22952962bf4a1047d591c8ee7a08433eb
Nadav Ivgi <nadav@shesek.info>
2018-04-11 23:37:30 +0300

    Docker: Don't start bitcoind when using an external lightningd instance
    Closes #31.

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -8,49 +8,51 @@ trap 'kill `jobs -p`' SIGTERM
 
 [[ "$NETWORK" == "mainnet" ]] && NETWORK=bitcoin
 
-if [ -d /etc/bitcoin ]; then
-  echo -n "Connecting to bitcoind configured in /etc/bitcoin... "
-
-  RPC_OPT="-datadir=/etc/bitcoin $([[ -z "$BITCOIND_RPCCONNECT" ]] || echo "-rpcconnect=$BITCOIND_RPCCONNECT")"
-
-elif [ -n "$BITCOIND_URI" ]; then
-  [[ "$BITCOIND_URI" =~ ^[a-z]+:\/+(([^:/]+):([^@/]+))@([^:/]+:[0-9]+)/?$ ]] || \
-    { echo >&2 "ERROR: invalid bitcoind URI: $BITCOIND_URI"; exit 1; }
-
-  echo -n "Connecting to bitcoind at ${BASH_REMATCH[4]}... "
-
-  RPC_OPT="-rpcconnect=${BASH_REMATCH[4]}"
-
-  if [ "${BASH_REMATCH[2]}" != "__cookie__" ]; then
-    RPC_OPT="$RPC_OPT -rpcuser=${BASH_REMATCH[2]} -rpcpassword=${BASH_REMATCH[3]}"
-  else
-    RPC_OPT="$RPC_OPT -datadir=/tmp/bitcoin"
-    [[ "$NETWORK" == "bitcoin" ]] && NET_PATH=/tmp/bitcoin || NET_PATH=/tmp/bitcoin/$NETWORK
-    mkdir -p $NET_PATH
-    echo "${BASH_REMATCH[1]}" > $NET_PATH/.cookie
-  fi
-
-else
-  echo -n "Starting bitcoind... "
-
-  mkdir -p /data/bitcoin
-  RPC_OPT="-datadir=/data/bitcoin"
-
-  bitcoind -$NETWORK $RPC_OPT $BITCOIND_OPTS &
-  echo -n "waiting for cookie... "
-  sed --quiet '/^\.cookie$/ q' <(inotifywait -e create,moved_to --format '%f' -qmr /data/bitcoin)
-fi
-
-echo -n "waiting for RPC... "
-bitcoin-cli -$NETWORK $RPC_OPT -rpcwait getblockchaininfo > /dev/null
-echo "ready."
-
-
 if [ -d /etc/lightning ]; then
   echo -n "Using lightningd directory mounted in /etc/lightning... "
   LN_PATH=/etc/lightning
 
 else
+
+  # Setup bitcoind (only needed when we're starting our own lightningd instance)
+  if [ -d /etc/bitcoin ]; then
+    echo -n "Connecting to bitcoind configured in /etc/bitcoin... "
+
+    RPC_OPT="-datadir=/etc/bitcoin $([[ -z "$BITCOIND_RPCCONNECT" ]] || echo "-rpcconnect=$BITCOIND_RPCCONNECT")"
+
+  elif [ -n "$BITCOIND_URI" ]; then
+    [[ "$BITCOIND_URI" =~ ^[a-z]+:\/+(([^:/]+):([^@/]+))@([^:/]+:[0-9]+)/?$ ]] || \
+      { echo >&2 "ERROR: invalid bitcoind URI: $BITCOIND_URI"; exit 1; }
+
+    echo -n "Connecting to bitcoind at ${BASH_REMATCH[4]}... "
+
+    RPC_OPT="-rpcconnect=${BASH_REMATCH[4]}"
+
+    if [ "${BASH_REMATCH[2]}" != "__cookie__" ]; then
+      RPC_OPT="$RPC_OPT -rpcuser=${BASH_REMATCH[2]} -rpcpassword=${BASH_REMATCH[3]}"
+    else
+      RPC_OPT="$RPC_OPT -datadir=/tmp/bitcoin"
+      [[ "$NETWORK" == "bitcoin" ]] && NET_PATH=/tmp/bitcoin || NET_PATH=/tmp/bitcoin/$NETWORK
+      mkdir -p $NET_PATH
+      echo "${BASH_REMATCH[1]}" > $NET_PATH/.cookie
+    fi
+
+  else
+    echo -n "Starting bitcoind... "
+
+    mkdir -p /data/bitcoin
+    RPC_OPT="-datadir=/data/bitcoin"
+
+    bitcoind -$NETWORK $RPC_OPT $BITCOIND_OPTS &
+    echo -n "waiting for cookie... "
+    sed --quiet '/^\.cookie$/ q' <(inotifywait -e create,moved_to --format '%f' -qmr /data/bitcoin)
+  fi
+
+  echo -n "waiting for RPC... "
+  bitcoin-cli -$NETWORK $RPC_OPT -rpcwait getblockchaininfo > /dev/null
+  echo "ready."
+
+  # Setup lightning
   echo -n "Starting lightningd... "
 
   LN_PATH=/data/lightning
