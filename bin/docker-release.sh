e4132e1602da60e2ff9b0058c1124d71e3965139
Nadav Ivgi <nadav@shesek.info>
2020-06-14 13:59:00 +0300

    docker release: Tag all images first, then push them
    So that everything will get tagged even if the upload gets interrupted.

--- bin/docker-release.sh
+++ bin/docker-release.sh
@@ -32,19 +32,16 @@ do
   docker manifest push $target -p
 done
 
-docker push $docker_name:$version-amd64
 
 docker tag $docker_name:$version-standalone-arm32v7 $docker_name:standalone-arm32v7
-docker push $docker_name:standalone-arm32v7
-
 docker tag $docker_name:$version-standalone-arm64v8 $docker_name:standalone-arm64v8
-docker push $docker_name:standalone-arm64v8
-
 docker tag $docker_name:$version-standalone-amd64 $docker_name:standalone-amd64
-docker push $docker_name:standalone-amd64
-
 docker tag $docker_name:$version-amd64 $docker_name:$version
-docker push $docker_name:$version
-
 docker tag $docker_name:$version-amd64 $docker_name:latest
+
+docker push $docker_name:$version-amd64
+docker push $docker_name:standalone-arm32v7
+docker push $docker_name:standalone-arm64v8
+docker push $docker_name:standalone-amd64
+docker push $docker_name:$version
 docker push $docker_name:latest
